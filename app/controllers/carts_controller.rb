e1b055dfd4ab39840b830fefc0835271585d5e5a
Jeff Gebert <jeffgebert@hotmail.com>
2019-12-13 17:40:53 +0000

    finished feature/user-auth

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,6 @@
 class CartsController < ApplicationController
+  before_filter :authorize
+
 
   def show
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
   get "/about" => "about#show"
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  
 
 
   resources :products, only: [:index, :show]
