362ac0d6c73a221da036a5f1b7d82e49e21ee70f
Jeff Gebert <jeffgebert@hotmail.com>
2019-12-17 21:17:59 +0000

    finished product details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature "users can navigate from the home page to the product detail page by clicking on a product", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        image: open_asset('apparel1.jpg'),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+    puts page.html
+    click_link(class: 'btn btn-default pull-right', match: :first)
+    puts "break"
+    puts page.html
+    sleep(1)
+    save_screenshot 'productDetail.png'
+
+    # VERIFY
+
+  end
+end
