f36652167c87a636da167ab26453a8abacfb43ec
Jeff Gebert <jeffgebert@hotmail.com>
2019-12-17 19:57:06 +0000

    finished feature spec home page

---
+++ spec/features/home_page_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        image: open_asset('apparel1.jpg'),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+
+    # DEBUG
+    save_screenshot 'products.png'
+
+    # VERIFY
+    expect(page).to have_css 'article.product', count: 10
+  end
+end
